0a6e623fb69d1965ccada20ffb0baf92419d1f13
RainbowMango <renhongcai@huawei.com>
2020-07-07 17:15:30 +0800

    drop me: add debug log

--- pkg/controller/util/federated_informer.go
+++ pkg/controller/util/federated_informer.go
@@ -294,7 +294,9 @@ func IsClusterContainsAPI(clusterConfig *restclient.Config, gvk schema.GroupVers
 		return false
 	}
 
+	klog.Infof("[JUSTFORDEBUG] kind: %s, apiVersion: %s, groups length: %d", groups.Kind, groups.APIVersion, len(groups.Groups))
 	for index := range groups.Groups {
+		klog.Infof("current group version kind: %s", groups.Groups[index].GroupVersionKind().String())
 		if groups.Groups[index].GroupVersionKind() == gvk {
 			return true
 		}
